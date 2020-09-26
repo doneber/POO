a762b2da49396799d1c974b9c4138000e1985031
doneber <52986565+doneber@users.noreply.github.com>
2020-09-26 15:59:46 -0400

    Sobrecarga de opoeradores en C#

---
+++ C#/2. SOBRECARGA POO/Fraccion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Sobrecarga
+{
+
+	public class Fraccion
+	{
+		private int numerador;
+		private int denominador;
+		/* SOBRECARGA DE CONSTRUCTORES */
+		public Fraccion()
+		{
+			this.numerador = 1;
+			denominador = 2;
+		}
+		public Fraccion(int n, int d)
+		{
+			this.numerador = n;
+			this.denominador = d;
+		}
+		/* SOBRECARGA DE METODOS */
+		public void mostrar(){
+			Console.WriteLine(numerador + " / " + denominador);
+		}
+		// Sobrecargar el metodo mostrar para o mostrar el numerador o denominador (true o false respectivamente)
+		public void mostrar(Boolean sw){
+			if(sw)
+				Console.WriteLine(this.numerador);
+			else
+				Console.WriteLine(this.denominador);
+		}
+		/* SOBRECARGA DE OPERADORES */
+		public static Fraccion operator +(Fraccion uno, Fraccion dos){
+			int auxNumerador = uno.numerador*dos.denominador + dos.numerador*uno.denominador;
+			int auxDenominador = uno.denominador*dos.denominador;
+			Fraccion resultado = new Fraccion(auxNumerador, auxDenominador);
+			return resultado;
+		}
+		public static Fraccion operator ~(Fraccion f){
+			return new Fraccion(f.numerador*f.numerador, f.denominador*f.denominador);
+		}
+		
+	}
+}
---
+++ C#/2. SOBRECARGA POO/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Sobrecarga
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Fraccion a = new Fraccion();
+			a.mostrar();
+			Fraccion b = new Fraccion(1,1);
+			b.mostrar(true);
+			b.mostrar(false);
+			Fraccion c = a+b;
+			Console.WriteLine("La suma es:");
+			c.mostrar();
+			Console.WriteLine("La multiplicación por si misma de la 1ra fracción es:");
+			Fraccion d = ~a;
+			d.mostrar();
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
